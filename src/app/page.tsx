ee9525e6978d275d140cfa9e5ac55637730cb271
teodie <54758584+teodie@users.noreply.github.com>
2026-03-04 16:58:24 +0800

    chore: remove unused import in app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,8 @@
-'use client'
-import Image from "next/image";
-import Content from "@/src/blog/test-blog.mdx"
 
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 dark:bg-black">
-      <Content />
+
     </div>
   );
 }
