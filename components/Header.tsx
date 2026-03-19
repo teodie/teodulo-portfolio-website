dd738d4759ecb422a6abcd854c27290e4553e5ec
teodie <54758584+teodie@users.noreply.github.com>
2026-03-19 13:33:19 +0800

    style: Change font size of the header for mobile devices

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,9 +7,9 @@ const Header = () => {
     md:flex-row md:h-full md:gap-0 md:text-start md:justify-between
     '>
 
-      <div className='pr-5  w-full'>
-        <h1 className='text-5xl mb-3 font-ovo'>Consider everything... but take nothing as a gospel.</h1>
-        <h3>- The Almanack of Naval Ravikant</h3>
+      <div className='pr-5  w-full '>
+        <h1 className='text-2xl md:text-5xl mb-3 font-ovo'>Consider everything... but take nothing as a gospel.</h1>
+        <h3 className='text-xs md:text-2xl'>- The Almanack of Naval Ravikant</h3>
       </div>
 
       <div className='e flex flex-col w-56 md:w-3xl items-center'>
